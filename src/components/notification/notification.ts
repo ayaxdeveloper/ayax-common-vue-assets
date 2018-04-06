093db9bc0266e5731f7f4816575f52bb5aa1f763
Толубаев Александр А <tolubaev_aa@ajax.ru>
2018-04-06 10:22:44 +0300

    edit notifications

--- src/components/notification/notification.ts
+++ src/components/notification/notification.ts
@@ -4,15 +4,6 @@ import { EventBus } from '../../event-bus';
 
 @Component
 export default class NotificationComponent extends Vue {
-    @Inject() notificationProvider: INotificationProvider;
-    notifications: NotificationItem[] = [];
-    created() {
-        EventBus.$on('addNotification', (notification: NotificationItem) => {
-            this.notifications.push(notification); 
-            if(notification.dismissAfter > 0 ) {
-                setInterval(()=>{notification.showing = false}, notification.dismissAfter);
-            }
-        });
-
-    }
+    @Prop() notificationProvider: INotificationProvider;
+    notifications: NotificationItem[] = this.notificationProvider.notifications;
 }
