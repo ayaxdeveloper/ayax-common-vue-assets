2eeea622c900fe6aff9eb8865bc5d90780c45b8c
Толубаев Александр А <tolubaev_aa@ajax.ru>
2018-07-19 18:50:55 +0300

    publish fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-var path = require('path');
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  mode: 'production',
+  entry: './src/Index.ts',
   output: {
-    path: path.resolve(__dirname, './dist/'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js',
     library: 'ayax-common-vue-assets',
     libraryTarget: 'umd'
-  },
-  externals: {
-    vue: 'vue'
   },
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader',
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        include: path.resolve(__dirname, 'src'),
         options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            css: 'vue-style-loader!css-loader',
-            scss: 'vue-style-loader!css-loader!sass-loader',
-            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
-          // other vue-loader options go here
+          appendTsSuffixTo: [/\.vue$/]
         }
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        include: path.resolve(__dirname, 'src'),
+        options: {
+          loaders: {
+            ts: 'ts-loader'
+          }
+        }
       },
       {
         test: /\.css$/,
@@ -43,20 +44,16 @@ module.exports = {
         ],
       },
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-        }
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'scss-loader'
+        ],
       },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
+      { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
     ]
   },
   resolve: {
@@ -65,44 +62,22 @@ module.exports = {
       vue$: 'vue/dist/vue.js'
     }
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map',
+  externals: [
+    'ayax-common-auth',
+    'ayax-common-cache',
+    'ayax-common-helpers',
+    'ayax-common-operation',
+    'ayax-common-types',
+    'element-ui',
+    'moment',
+    'vue-shortkey',
+    'vuedraggable',
+    'vuetify'
+  ],
   plugins: [
-    new VueLoaderPlugin()
-  ]
-};
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map';
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
+    new VueLoaderPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
+      PRODUCTION: JSON.stringify(true),
     })
-  ]);
-} 
+  ]
+}
