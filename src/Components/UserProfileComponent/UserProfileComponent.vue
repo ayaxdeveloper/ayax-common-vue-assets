d0b3349de1e5bf6e9143689f5559062e4a057a1f
karnaukhov_vo <karnaukhov_vo@ayax.ru>
2018-12-26 10:33:35 +0300

    added prop to usercomponent for showing avatar

--- src/Components/UserProfileComponent/UserProfileComponent.vue
+++ src/Components/UserProfileComponent/UserProfileComponent.vue
@@ -1,29 +1,25 @@
 <template>
-    <v-menu 
-    offset-y
-    bottom>
-        <v-btn
-        slot="activator"
-        flat 
-        >
-            <v-avatar
-            class="mr-3"
-            size="32"
-            v-if="currentUser && currentUser.profilePictureUrl"
-            >
-            <img :src="currentUser.profilePictureUrl" alt="avatar">
-            </v-avatar>
-            {{currentUser && currentUser.name}}
-            <v-icon>mdi-menu-down</v-icon>
-        </v-btn>
-        <v-list
-        dark>
-            <v-list-tile @click="logout()">
-                <v-list-tile-action><v-icon>mdi-logout</v-icon></v-list-tile-action>
-                <v-list-tile-title>Выйти</v-list-tile-title>
-            </v-list-tile>
-        </v-list>
-    </v-menu>
+  <v-menu offset-y bottom>
+    <v-btn slot="activator" flat>
+      <v-avatar
+        class="mr-3"
+        size="32"
+        v-if="currentUser && currentUser.profilePictureUrl && showAvatar"
+      >
+        <img :src="currentUser.profilePictureUrl" alt="avatar">
+      </v-avatar>
+      {{currentUser && currentUser.name}}
+      <v-icon>mdi-menu-down</v-icon>
+    </v-btn>
+    <v-list dark>
+      <v-list-tile @click="logout()">
+        <v-list-tile-action>
+          <v-icon>mdi-logout</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Выйти</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
 </template>
 
 <script lang="ts">
@@ -31,20 +27,21 @@ import { AuthUser, IAuthService, ITokenService } from "ayax-common-auth";
 import { Component, Inject, Prop, Vue } from "vue-property-decorator";
 
 @Component({
-    name: "a-user-profile"
+  name: "a-user-profile"
 })
 export default class UserProfileComponent extends Vue {
-    @Inject() authService: IAuthService;
-    @Inject() eventBus: Vue;
-    @Inject() tokenService: ITokenService;
-    @Prop({default: null}) modules?: string[];
-    currentUser: AuthUser = new AuthUser();
-    async created() {
-        this.currentUser = (await this.authService.GetCurrentUser());
-    }
+  @Inject() authService: IAuthService;
+  @Inject() eventBus: Vue;
+  @Inject() tokenService: ITokenService;
+  @Prop({ default: null }) modules?: string[];
+  @Prop({ default: true }) showAvatar: boolean;
+  currentUser: AuthUser = new AuthUser();
+  async created() {
+    this.currentUser = await this.authService.GetCurrentUser();
+  }
 
-    logout() {
-        this.eventBus.$emit("logout");
-    }
+  logout() {
+    this.eventBus.$emit("logout");
+  }
 }
 </script>
