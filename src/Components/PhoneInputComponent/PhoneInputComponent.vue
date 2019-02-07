d73785c04fc251e1ee0fa13172307732dc993f61
Alexander Tolubaev <tolubaev_aa@ayax.ru>
2019-02-07 11:12:21 +0300

    phone input fix

--- src/Components/PhoneInputComponent/PhoneInputComponent.vue
+++ src/Components/PhoneInputComponent/PhoneInputComponent.vue
@@ -18,76 +18,77 @@ import Vue from "vue";
 import { Component, Prop, Watch, Emit } from "vue-property-decorator";
 
 @Component({
-    name: "a-phone-input"
+  name: "a-phone-input"
 })
 export default class PhoneInputComponent extends Vue {
-    @Prop({ type: String, default: "" }) value;
+  @Prop({ type: String }) value;
+  @Prop({ type: Number, default: 0 }) forceFormatting: number;
 
-    formattedValue: string = this.value.toString();
+  formattedValue: string = this.value.toString();
 
-    created() {
-        this.formatInput(true);
-    }
+  @Watch("forceFormatting")
+  onForce() {
+    this.formatInput(true);
+  }
 
-    mounted() {
-        while (this.formattedValue.length !== 17) {
-            this.formatInput(true);
+  created() {
+    this.formatInput(true);
+  }
+
+  mounted() {
+    setTimeout(() => this.formatInput(true), 500);
+  }
+
+  formatInput(initial = false) {
+    const inputEvent = event as any;
+    if (
+      !inputEvent ||
+      (inputEvent.inputType &&
+        inputEvent.inputType !== "deleteContentBackward" &&
+        inputEvent.inputType !== "deleteContentForward")
+    ) {
+      let inputValue = this.formattedValue.replace(/\D/g, "");
+
+      if (initial) {
+        if (inputValue.length < 11) {
+          inputValue = "8" + inputValue.substring(0, inputValue.length);
         }
-    }
+      }
 
-    formatInput(initial = false) {
-        const inputEvent = event as any;
+      if (this.formattedValue) {
         if (
-            !inputEvent ||
-            (inputEvent.inputType &&
-                inputEvent.inputType !== "deleteContentBackward" &&
-                inputEvent.inputType !== "deleteContentForward")
+          inputEvent &&
+          inputEvent.inputType &&
+          inputEvent.inputType === "insertFromPaste"
         ) {
-            let inputValue = this.formattedValue.replace(/\D/g, "");
-
-            if (initial) {
-                if (inputValue.length < 11) {
-                    inputValue = "8" + inputValue.substring(0, inputValue.length);
-                }
-            }
-
-            if (this.formattedValue) {
-                if (
-                    inputEvent &&
-                    inputEvent.inputType &&
-                    inputEvent.inputType === "insertFromPaste"
-                ) {
-                    let pasteValue = inputEvent.target.value.replace(/\D/g, "");
-                    if (pasteValue.length < 11) {
-                        inputValue =
-                            "8" + inputValue.substring(0, inputValue.length);
-                    }
-                }
-
-                const reg = /^(\d{1})(\d{3})(\d{3})?(\d{2})?(\d{2})?/;
-                inputValue = inputValue.replace(reg, "8 ($2) $3-$4-$5");
-
-                this.formattedValue = inputValue;
-
-                setTimeout(() => {
-                    this.$refs["phone-input"]["$refs"][
-                        "input"
-                    ].value = inputValue;
-                }, 0);
-            }
+          let pasteValue = inputEvent.target.value.replace(/\D/g, "");
+          if (pasteValue.length < 11) {
+            inputValue = "8" + inputValue.substring(0, inputValue.length);
+          }
         }
-    }
 
-    @Watch("formattedValue")
-    onFormat() {
-        let result = "";
-        if (this.formattedValue) {
-            result = this.formattedValue.replace(/\D/g, "");
-        }
-        this.emitValue(result);
-    }
+        const reg = /^(\d{1})(\d{3})(\d{3})?(\d{2})?(\d{2})?/;
+        inputValue = inputValue.replace(reg, "8 ($2) $3-$4-$5");
 
-    @Emit("input")
-    emitValue(value) {}
+        this.formattedValue = inputValue;
+
+        setTimeout(() => {
+          this.$refs["phone-input"]["$refs"]["input"].value = inputValue;
+        }, 0);
+      }
+    }
+  }
+
+  @Watch("formattedValue")
+  onFormat() {
+    let result = "";
+    if (this.formattedValue) {
+      result = this.formattedValue.replace(/\D/g, "");
+    }
+    this.emitValue(result);
+  }
+
+  @Emit("input")
+  emitValue(value) {}
 }
 </script>
