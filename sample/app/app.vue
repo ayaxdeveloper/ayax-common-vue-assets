6d94962d62a989647612bbbddd85a56f2116f581
Alexander Tolubaev <tolubaev_aa@ayax.ru>
2019-02-07 18:25:58 +0300

    added search to sidebar

--- sample/app/app.vue
+++ sample/app/app.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="app">
     <v-app>
-      <a-sidebar v-model="sidebarMiniProp" :items="sidebarItems">
-            <v-divider></v-divider>
-            <v-list dense>
-                <v-list-tile>
-                    <v-list-tile-content>
-                        <v-list-tile-title>slot in sidebar</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
+      <a-sidebar v-model="sidebarMiniProp" :searchbar="true" :items="sidebarItems">
+        <v-divider></v-divider>
+        <v-list dense>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>slot in sidebar</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
       </a-sidebar>
       <v-content>
         <a-notification :notificationProvider="notificationProvider"></a-notification>
         <v-toolbar dense fixed app>
           <v-toolbar-side-icon>
-              <v-icon x-large @click="sidebarMiniProp = !sidebarMiniProp">{{ sidebarMiniProp ? 'mdi-chevron-right' : 'mdi-chevron-left'}}</v-icon>
+            <v-icon
+              x-large
+              @click="sidebarMiniProp = !sidebarMiniProp"
+            >{{ sidebarMiniProp ? 'mdi-chevron-right' : 'mdi-chevron-left'}}</v-icon>
           </v-toolbar-side-icon>
-           <a-breadcrumbs :breadcrumbsNames="breadcrumbsNames"></a-breadcrumbs>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-              <v-btn small color="success" @click="castNotification('success')">notification</v-btn>
-              <v-btn small color="error" @click="castNotification('error')">notification</v-btn>
-              <v-btn small color="info" @click="castNotification('info')">notification</v-btn>
-              <v-btn small color="warning" @click="castNotification('warning')">notification</v-btn>
-            </v-toolbar-items>
-      </v-toolbar>
+          <a-breadcrumbs :breadcrumbsNames="breadcrumbsNames"></a-breadcrumbs>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn small color="success" @click="castNotification('success')">notification</v-btn>
+            <v-btn small color="error" @click="castNotification('error')">notification</v-btn>
+            <v-btn small color="info" @click="castNotification('info')">notification</v-btn>
+            <v-btn small color="warning" @click="castNotification('warning')">notification</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
         <v-container fluid class="px-3 py-4">
           <router-view></router-view>
         </v-container>
@@ -41,84 +44,108 @@ import { VueInjection } from "../Injection";
 
 @Component
 export default class App extends VueInjection {
-    sidebarMiniProp = false;
-    
-    breadcrumbsNames = {
-        "": {name: "Главная", route: "home"},
-        "switcher": {name: "Вертушка", route: "switcher-list"},
-        "route": {name: "Маршруты", route: "route"},
-        "contacttype": {name: "Типы контактов", route: "contacttype-list"},
-        "config": {name: "Настройки" },
-        "help": {name: "Помощь", route: "help" },
-        "dictionary": {name: "Справочники"},
-        "list": {name: "Список"},
-        "board": {name: "Доска"},
-        "edit": {name: "Редактирование"},
-        "card": {name: "Карточка"}
-    };
+  sidebarMiniProp = false;
 
-    sidebarItems: SidebarComponentItem[] = [
+  breadcrumbsNames = {
+    "": { name: "Главная", route: "home" },
+    switcher: { name: "Вертушка", route: "switcher-list" },
+    route: { name: "Маршруты", route: "route" },
+    contacttype: { name: "Типы контактов", route: "contacttype-list" },
+    config: { name: "Настройки" },
+    help: { name: "Помощь", route: "help" },
+    dictionary: { name: "Справочники" },
+    list: { name: "Список" },
+    board: { name: "Доска" },
+    edit: { name: "Редактирование" },
+    card: { name: "Карточка" }
+  };
+
+  sidebarItems: SidebarComponentItem[] = [
+    new SidebarComponentItem({
+      title: "Главная",
+      icon: "mdi-book-open-variant",
+      href: "/",
+      isSystem: true
+    }),
+    new SidebarComponentItem({
+      title: "TABLE",
+      icon: "mdi-format-list-checkbox",
+      route: "/table-test"
+    }),
+    new SidebarComponentItem({
+      title: "НАСТРОЙКИ",
+      icon: "mdi-wrench",
+      subItems: [
         new SidebarComponentItem({
-            title: "Главная",
-            icon: "mdi-book-open-variant",
-            href: "/",
-            isSystem: true
+          title: "Вертушка",
+          route: "/switcher/list"
         }),
-        new SidebarComponentItem({title: "TABLE", icon: "mdi-format-list-checkbox", route: "/table-test"}),
+        new SidebarComponentItem({ title: "Маршруты", route: "/route" }),
+        new SidebarComponentItem({ title: "Источники", route: "/infosource" }),
         new SidebarComponentItem({
-            title: "НАСТРОЙКИ",
-            icon: "mdi-wrench",
-            subItems: [
-                new SidebarComponentItem({title: "Вертушка", route: "/switcher/list"}),
-                new SidebarComponentItem({title: "Маршруты", route: "/route"}),
-                new SidebarComponentItem({title: "Источники", route: "/infosource"}),
-                new SidebarComponentItem({title: "Темы обращений", route: "/leadtype"}),
-                new SidebarComponentItem({title: "Типы объектов", route: "/objecttype"}),
-                new SidebarComponentItem({title: "Номера операторов", route: "/operatornumber/list"}),
-                new SidebarComponentItem({title: "Номера подразделений", route: "/divisionredirect"}),
-                new SidebarComponentItem({title: "Параметры", route: "/preference"})
-            ]
-        })
-    ];
+          title: "Темы обращений",
+          route: "/leadtype"
+        }),
+        new SidebarComponentItem({
+          title: "Типы объектов",
+          route: "/objecttype"
+        }),
+        new SidebarComponentItem({
+          title: "Номера операторов",
+          route: "/operatornumber/list"
+        }),
+        new SidebarComponentItem({
+          title: "Номера подразделений",
+          route: "/divisionredirect"
+        }),
+        new SidebarComponentItem({ title: "Параметры", route: "/preference" })
+      ]
+    })
+  ];
 
-    castNotification(type: string) {
-        switch (type){
-            case "success": {
-                this.notificationProvider.Success("Успешно сохранено");
-            }
-            break;
-            case "error": {
-                this.notificationProvider.Error();
-            }
-            break;
-            case "info": {
-                this.notificationProvider.Info();
-            }
-            break;
-            case "warning": {
-                this.notificationProvider.Warning();
-            }
-            break;
-            default: break;
+  castNotification(type: string) {
+    switch (type) {
+      case "success":
+        {
+          this.notificationProvider.Success("Успешно сохранено");
         }
+        break;
+      case "error":
+        {
+          this.notificationProvider.Error();
+        }
+        break;
+      case "info":
+        {
+          this.notificationProvider.Info();
+        }
+        break;
+      case "warning":
+        {
+          this.notificationProvider.Warning();
+        }
+        break;
+      default:
+        break;
     }
+  }
 
-    created() {
-        // Проставим текущего пользователя
-        const currentUserObject = {
-            accessRules:[],
-            accessRulesNames:[],
-            birthdate:"1988-02-27T00:00:00",
-            id:0,
-            identity:null,
-            login:"ajax\\as_testuser",
-            name:"Тестовый пользователь",
-            profilePictureUrl:null,
-            subdivisionIds:[11, 14],
-            uid:"00000000-0000-0000-0000-000000000000"
-        }
-        localStorage.setItem("currentUser", JSON.stringify(currentUserObject));
-    }
+  created() {
+    // Проставим текущего пользователя
+    const currentUserObject = {
+      accessRules: [],
+      accessRulesNames: [],
+      birthdate: "1988-02-27T00:00:00",
+      id: 0,
+      identity: null,
+      login: "ajax\\as_testuser",
+      name: "Тестовый пользователь",
+      profilePictureUrl: null,
+      subdivisionIds: [11, 14],
+      uid: "00000000-0000-0000-0000-000000000000"
+    };
+    localStorage.setItem("currentUser", JSON.stringify(currentUserObject));
+  }
 }
 </script>                            
 
--- src/Components/SidebarComponent/SidebarComponent.vue
+++ src/Components/SidebarComponent/SidebarComponent.vue
@@ -1,107 +1,119 @@
 <template>
-    <v-navigation-drawer
-        class="sidebarComponent"
-        app
-        :dark="darkTheme"
-        :mini-variant.sync="mini"
-        mini-variant-width="60"
-        permanent
-        fixed
-        :width="width"
-    >
-        <v-layout justify-center>
-            <div v-show="!mini" class="userPhotoContainer mt-3">
-                <v-card flat>
-                    <img
-                        class="userPhoto mx-auto"
-                        :src="currentUser.profilePictureUrl ? currentUser.profilePictureUrl : noAvatarImage"
-                        alt="avatar"
-                    >
-                </v-card>
-                <div
-                    class="mt-2"
-                    style="text-align: center; color: #fff; font-size: 13px"
-                >{{currentUser && currentUser.name}}</div>
-            </div>
-        </v-layout>
-        <v-layout>
-            <v-flex class="sidebar-content">
-                <v-list dense>
-                    <v-list-group
-                        v-for="item in items.filter(x=>x.isSystem && x.visible)"
-                        :key="item.title"
-                        :prepend-icon="item.icon"
-                        append-icon
-                        :value="item.expanded"
-                        @click="click($event, item)"
-                        @click.middle="clickMiddle($event, item)"
-                    >
-                        <v-list-tile slot="activator">
-                            <v-list-tile-content>
-                                <v-list-tile-title :title="item.title">{{ item.title }}</v-list-tile-title>
-                            </v-list-tile-content>
-                            <v-list-tile-action v-if="item.subItems.length > 0">
-                                <v-btn icon @click.stop="toogleList(item)">
-                                    <v-icon>{{ item.arrowDirection }}</v-icon>
-                                </v-btn>
-                            </v-list-tile-action>
-                        </v-list-tile>
-                        <v-list-tile
-                            v-if="item.subItems.length > 0"
-                            v-for="subItem in item.subItems.filter(subItem => subItem.visible)"
-                            :key="subItem.title"
-                            @click="click($event, subItem)"
-                        >
-                            <v-list-tile-action @click.middle="clickMiddle($event, subItem)">
-                                <v-icon>{{ subItem.icon }}</v-icon>
-                            </v-list-tile-action>
-                            <v-list-tile-content @click.middle="clickMiddle($event, subItem)">
-                                <v-list-tile-title :title="subItem.title">{{ subItem.title }}</v-list-tile-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list-group>
-                </v-list>
-            </v-flex>
-        </v-layout>
-        <v-divider></v-divider>
+  <v-navigation-drawer
+    class="sidebarComponent"
+    app
+    :dark="darkTheme"
+    :mini-variant.sync="mini"
+    mini-variant-width="60"
+    permanent
+    fixed
+    :width="width"
+  >
+    <v-layout justify-center>
+      <div v-show="!mini" class="userPhotoContainer mt-3">
+        <v-card flat>
+          <img
+            class="userPhoto mx-auto"
+            :src="currentUser.profilePictureUrl ? currentUser.profilePictureUrl : noAvatarImage"
+            alt="avatar"
+          >
+        </v-card>
+        <div
+          class="mt-2"
+          style="text-align: center; color: #fff; font-size: 13px"
+        >{{currentUser && currentUser.name}}</div>
+      </div>
+    </v-layout>
+    <v-layout>
+      <v-flex class="sidebar-content">
         <v-list dense>
-            <v-list-group
-                v-for="item in items.filter(item => !item.isSystem && item.visible)"
-                :key="item.title"
-                :prepend-icon="item.icon"
-                append-icon
-                :value="item.expanded"
-                :class="[item.selected ? 'selected' : '']"
-                @click="click($event, item)"
-                @click.middle="clickMiddle($event, item)"
+          <v-list-group
+            v-for="item in items.filter(x=>x.isSystem && x.visible)"
+            :key="item.title"
+            :prepend-icon="item.icon"
+            append-icon
+            :value="item.expanded"
+            @click="click($event, item)"
+            @click.middle="clickMiddle($event, item)"
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title :title="item.title">{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action v-if="item.subItems.length > 0">
+                <v-btn icon @click.stop="toogleList(item)">
+                  <v-icon>{{ item.arrowDirection }}</v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-list-tile
+              v-if="item.subItems.length > 0"
+              v-for="subItem in item.subItems.filter(subItem => subItem.visible)"
+              :key="subItem.title"
+              @click="click($event, subItem)"
             >
-                <v-list-tile slot="activator">
-                    <v-list-tile-content>
-                        <v-list-tile-title :title="item.title">{{ item.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                    <v-list-tile-action v-if="item.subItems.length > 0">
-                        <v-btn icon @click.stop="toogleList(item)">
-                            <v-icon>{{ item.arrowDirection }}</v-icon>
-                        </v-btn>
-                    </v-list-tile-action>
-                </v-list-tile>
-                <v-list-tile
-                    v-for="subItem in item.subItems"
-                    :key="subItem.title"
-                    :class="[subItem.selected ? 'selected' : '']"
-                    @click="click($event, subItem)"
-                >
-                    <v-list-tile-action @click.middle="clickMiddle($event, subItem)">
-                        <v-icon>{{ subItem.icon }}</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content @click.middle="clickMiddle($event, subItem)">
-                        <v-list-tile-title :title="subItem.title">{{ subItem.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list-group>
+              <v-list-tile-action @click.middle="clickMiddle($event, subItem)">
+                <v-icon>{{ subItem.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content @click.middle="clickMiddle($event, subItem)">
+                <v-list-tile-title :title="subItem.title">{{ subItem.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
         </v-list>
-        <slot></slot>
-    </v-navigation-drawer>
+      </v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+    <v-text-field
+      v-if="searchbar"
+      class="mx-3 my-2"
+      style="padding-top: 0"
+      v-model="searchQuery"
+      :placeholder="searchbarPlaceholder"
+      hide-details
+      append-icon="mdi-magnify"
+      single-line
+      clearable
+      @click:clear="clearSearch"
+    ></v-text-field>
+    <v-list dense>
+      <v-list-group
+        v-for="item in items.filter(item => !item.isSystem && item.visible && searchResult(item))"
+        :key="item.title"
+        :prepend-icon="item.icon"
+        append-icon
+        :value="item.expanded"
+        :class="[item.selected ? 'selected' : '']"
+        @click="click($event, item)"
+        @click.middle="clickMiddle($event, item)"
+      >
+        <v-list-tile slot="activator">
+          <v-list-tile-content>
+            <v-list-tile-title :title="item.title">{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action v-if="item.subItems.length > 0">
+            <v-btn icon @click.stop="toogleList(item)">
+              <v-icon>{{ item.arrowDirection }}</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile
+          v-for="subItem in item.subItems.filter(x => x.visible && searchResult(x))"
+          :key="subItem.title"
+          :class="[subItem.selected ? 'selected' : '']"
+          @click="click($event, subItem)"
+        >
+          <v-list-tile-action @click.middle="clickMiddle($event, subItem)">
+            <v-icon>{{ subItem.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content @click.middle="clickMiddle($event, subItem)">
+            <v-list-tile-title :title="subItem.title">{{ subItem.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list-group>
+    </v-list>
+    <slot></slot>
+  </v-navigation-drawer>
 </template>
 
 <script lang="ts">
@@ -112,189 +124,225 @@ import { OpenInNewWindowMixin } from "../../Mixins/OpenInNewWindow/OpenInNewWind
 import { SidebarComponentItem } from "./SidebarItem";
 
 @Component({
-    name: "a-sidebar"
+  name: "a-sidebar"
 })
 export default class SidebarComponent extends Vue {
-    @Inject() authService: IAuthService;
-    @Prop({ default: () => [] }) modules: string[];
-    @Prop() items: SidebarComponentItem[];
-    @Prop({ default: true }) darkTheme: boolean;
-    @Prop({ default: 256 }) width: number;
-    @Prop({ default: false }) value: boolean;
-    mini = this.value;
-    currentUser: AuthUser = new AuthUser();
-    noAvatarImage = require("../../assets/image/no_avatar_image.png");
+  @Inject() authService: IAuthService;
+  @Prop({ default: () => [] }) modules: string[];
+  @Prop() items: SidebarComponentItem[];
+  @Prop({ default: true }) darkTheme: boolean;
+  @Prop({ default: 256 }) width: number;
+  @Prop({ default: false }) searchbar: boolean;
+  @Prop({ default: "Поиск раздела..." }) searchbarPlaceholder: string;
+  @Prop({ default: false }) value: boolean;
+  mini = this.value;
+  currentUser: AuthUser = new AuthUser();
+  noAvatarImage = require("../../assets/image/no_avatar_image.png");
 
-    async created() {
-        this.currentUser = await this.authService.GetCurrentUser();
-        this.$router.afterEach((to, from) => {
-            this.FillActiveItemFromRoute(to.path);
-        });
-    }
+  searchQuery = "";
 
-    mounted() {
-        this.FillActiveItemFromRoute(this.$router.currentRoute.path);
-    }
-
-    FillActiveItemFromRoute(routePath: string) {
-        this.items.forEach(element => {
-            element.selected = false;
-            element.subItems.forEach(subElement => {
-                subElement.selected = false;
-            });
-        });
-        this.items
-            .filter(x => !x.isSystem)
-            .some(item => {
-                if (item.route === routePath) {
-                    item.selected = true;
-                    return true;
-                } else if (item.subItems && item.subItems.length > 0) {
-                    if (
-                        item.subItems.some(
-                            subItem =>
-                                (subItem.selected = subItem.route === routePath)
-                        )
-                    ) {
-                        item.expanded = true;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-                return false;
-            });
-    }
-
-    @Watch("value")
-    onChange() {
-        this.mini = this.value;
-    }
-
-    @Watch("mini")
-    onStateChanged(val: boolean, oldVal: boolean) {
-        this.$emit("input", this.mini);
-        if (val === true) {
-            this.items.forEach(element => {
-                element.expanded = false;
-                element.arrowDirection = "mdi-chevron-down";
-            });
+  searchResult(item: SidebarComponentItem) {
+    if (this.searchQuery.length > 0) {
+      if (item.subItems.length > 0) {
+        if (
+          item.subItems.filter(x =>
+            x.title
+              .toLowerCase()
+              .includes(this.searchQuery.toLowerCase().trim())
+          ).length > 0
+        ) {
+          item.expanded = true;
+          return true;
+        } else {
+          return false;
         }
+      } else if (
+        item.title.toLowerCase().includes(this.searchQuery.toLowerCase().trim())
+      ) {
+        return true;
+      } else {
+        return false;
+      }
     }
+    item.expanded = false;
+    return true;
+  }
 
-    toogleList(item: SidebarComponentItem) {
-        this.items.forEach(element => {
-            if (element !== item) {
-                element.expanded = false;
-                element.arrowDirection = "mdi-chevron-down";
-            }
-        });
+  async created() {
+    this.currentUser = await this.authService.GetCurrentUser();
+    this.$router.afterEach((to, from) => {
+      this.FillActiveItemFromRoute(to.path);
+    });
+  }
 
-        item.expanded = !item.expanded;
-        item.arrowDirection === "mdi-chevron-down"
-            ? (item.arrowDirection = "mdi-chevron-up")
-            : (item.arrowDirection = "mdi-chevron-down");
-    }
+  mounted() {
+    this.FillActiveItemFromRoute(this.$router.currentRoute.path);
+  }
 
-    click(e, item: SidebarComponentItem) {
-        e = e || window.event;
-        if (item.route == null && item.href == null) {
-            this.toogleList(item);
-            this.mini = false;
-        } else if (item.route != null) {
-            if (item.newTab || e.ctrlKey || e.which === 2 || e.button === 4) {
-                window.open(item.route);
-            } else {
-                this.$router.push({ path: item.route });
-            }
-        } else if (item.href != null) {
-            if (item.newTab || e.ctrlKey || e.which === 2 || e.button === 4) {
-                window.open(item.href);
-            } else {
-                location.href = item.href;
-            }
+  clearSearch() {
+    this.$nextTick(() => {
+      this.searchQuery = "";
+    });
+  }
+
+  FillActiveItemFromRoute(routePath: string) {
+    this.items.forEach(element => {
+      element.selected = false;
+      element.subItems.forEach(subElement => {
+        subElement.selected = false;
+      });
+    });
+    this.items
+      .filter(x => !x.isSystem)
+      .some(item => {
+        if (item.route === routePath) {
+          item.selected = true;
+          return true;
+        } else if (item.subItems && item.subItems.length > 0) {
+          if (
+            item.subItems.some(
+              subItem => (subItem.selected = subItem.route === routePath)
+            )
+          ) {
+            item.expanded = true;
+            return true;
+          } else {
+            return false;
+          }
         }
-    }
+        return false;
+      });
+  }
 
-    clickMiddle(e, item: SidebarComponentItem) {
-        e.preventDefault();
-        if (item.route != null) {
-            window.open(item.route);
-            return;
-        } else if (item.href != null) {
-            window.open(item.route);
-            return;
-        }
-        this.click(e, item);
-    }
+  @Watch("value")
+  onChange() {
+    this.mini = this.value;
+  }
 
-    moveToNewWindow(item: SidebarComponentItem) {
-        if (item.route == null && item.href == null) {
-            this.toogleList(item);
-            this.mini = false;
-        } else if (item.route != null) {
-            window.open(item.route);
-        } else if (item.href != null) {
-            window.open(item.href);
-        }
+  @Watch("mini")
+  onStateChanged(val: boolean, oldVal: boolean) {
+    this.$emit("input", this.mini);
+    if (val === true) {
+      this.items.forEach(element => {
+        element.expanded = false;
+        element.arrowDirection = "mdi-chevron-down";
+      });
     }
+  }
+
+  toogleList(item: SidebarComponentItem) {
+    this.items.forEach(element => {
+      if (element !== item) {
+        element.expanded = false;
+        element.arrowDirection = "mdi-chevron-down";
+      }
+    });
+
+    item.expanded = !item.expanded;
+    item.arrowDirection === "mdi-chevron-down"
+      ? (item.arrowDirection = "mdi-chevron-up")
+      : (item.arrowDirection = "mdi-chevron-down");
+  }
+
+  click(e, item: SidebarComponentItem) {
+    e = e || window.event;
+    if (item.route == null && item.href == null) {
+      this.toogleList(item);
+      this.mini = false;
+    } else if (item.route != null) {
+      if (item.newTab || e.ctrlKey || e.which === 2 || e.button === 4) {
+        window.open(item.route);
+      } else {
+        this.$router.push({ path: item.route });
+      }
+    } else if (item.href != null) {
+      if (item.newTab || e.ctrlKey || e.which === 2 || e.button === 4) {
+        window.open(item.href);
+      } else {
+        location.href = item.href;
+      }
+    }
+  }
+
+  clickMiddle(e, item: SidebarComponentItem) {
+    e.preventDefault();
+    if (item.route != null) {
+      window.open(item.route);
+      return;
+    } else if (item.href != null) {
+      window.open(item.route);
+      return;
+    }
+    this.click(e, item);
+  }
+
+  moveToNewWindow(item: SidebarComponentItem) {
+    if (item.route == null && item.href == null) {
+      this.toogleList(item);
+      this.mini = false;
+    } else if (item.route != null) {
+      window.open(item.route);
+    } else if (item.href != null) {
+      window.open(item.href);
+    }
+  }
 }
 </script>
 
 <style scoped>
 .sidebarComponent::-webkit-scrollbar {
-    width: 8px;
+  width: 8px;
 }
 
 .sidebarComponent::-webkit-scrollbar-thumb {
-    background-color: #ccc;
-    border-radius: 8px;
-    border: 2px solid #464646;
+  background-color: #ccc;
+  border-radius: 8px;
+  border: 2px solid #464646;
 }
 
 .sidebar-switcher {
-    max-width: 60px;
+  max-width: 60px;
 }
 
 .list__tile__action {
-    min-width: 32px !important;
+  min-width: 32px !important;
 }
 
 .list__tile__title {
-    color: #ffffff;
+  color: #ffffff;
 }
 .collapseBtn {
-    margin-left: 12px;
+  margin-left: 12px;
 }
 
 .userPhoto {
-    display: block;
-    max-width: 100%;
-    height: auto;
-    width: auto;
-    max-height: 144px;
-    border: 1px solid #fff;
+  display: block;
+  max-width: 100%;
+  height: auto;
+  width: auto;
+  max-height: 144px;
+  border: 1px solid #fff;
 }
 
 .userPhotoContainer {
-    width: 224px;
+  width: 224px;
 }
 
 .v-list__group.selected {
-    background-color: rgb(250, 250, 250);
-    color: rgba(0, 0, 0, 0.87);
+  background-color: rgb(250, 250, 250);
+  color: rgba(0, 0, 0, 0.87);
 }
 
 .v-list__group.selected >>> .v-icon {
-    color: rgba(0, 0, 0, 0.87);
+  color: rgba(0, 0, 0, 0.87);
 }
 
 .selected {
-    background-color: rgb(250, 250, 250);
-    color: rgba(0, 0, 0, 0.87);
+  background-color: rgb(250, 250, 250);
+  color: rgba(0, 0, 0, 0.87);
 }
 
 .selected >>> .v-icon {
-    color: rgba(0, 0, 0, 0.87);
+  color: rgba(0, 0, 0, 0.87);
 }
 </style>
